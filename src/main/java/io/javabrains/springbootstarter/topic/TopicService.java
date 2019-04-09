01380d371d70a85051baf32d811c1f65c81f1d49
Zdravko Mavkov <mavkov@hotmail.com>
2019-04-09 09:04:51 +0200

    first commit

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,41 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Core Java", "Core Java Description"),
+            new Topic("javascript", "Java Script", "Java Script Description")
+    ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+//        for(int i = 0; i < topics.size(); i++){
+//            Topic t = topics.get(i);
+//            if(t.getId().equals(id)){
+//                topics.set(i, topic);
+//                return;
+//            }
+//        }
+        IntStream.range(0, topics.size()).filter(i -> topics.get(i).getId().equals(id));
+    }
+}
